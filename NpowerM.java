b62001094da8f9db2d2afe94b326fcc2ba5f1ca3
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-05-03 15:27:12 +0300

    Create NpowerM.java

---
+++ NpowerM.java
@@ -0,0 +1,11 @@
+public class NpowerM {
+    public static void main(String[] args) {
+        exponent(2, 8);
+        exponent(3, 4);
+    }
+
+    public static int exponent(int number, int power) {
+        if (power == 0) return 1;
+        return number*exponent(number, power - 1);
+    }
+}
