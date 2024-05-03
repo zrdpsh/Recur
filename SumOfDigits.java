6f52eb93c591fbb1c6ef3f5e01e686198045bbdc
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-05-03 15:28:16 +0300

    Create SumOfDigits.java

---
+++ SumOfDigits.java
@@ -0,0 +1,21 @@
+public class SumOfDigits {
+    public static void main(String[] args) {
+        System.out.println(sumOfDigits(111));
+        System.out.println(sumOfDigits(11));
+        System.out.println(sumOfDigits(1000000000));
+        System.out.println(sumOfDigits(1000000001));
+        System.out.println(sumOfDigits(0));
+        System.out.println(sumOfDigits(-1));
+        System.out.println(sumOfDigits(171717));
+
+    }
+
+    public static int sumOfDigits(int exploredNumber) {
+        int lastDigit = exploredNumber % 10;
+
+        int dividedNumber = exploredNumber / 10;
+        if (dividedNumber == 0) return lastDigit;
+
+        return lastDigit + sumOfDigits(dividedNumber);
+    }
+}
