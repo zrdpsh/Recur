71ec6f100096c09f66afca5a5d1733a04ec29b8f
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-05-05 16:45:03 +0300

    Create StringIsPalindrome.java

---
+++ StringIsPalindrome.java
@@ -0,0 +1,21 @@
+public class StringIsPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(""));
+        System.out.println(isPalindrome("H"));
+        System.out.println(isPalindrome("aba"));
+        System.out.println(isPalindrome("google"));
+        System.out.println(isPalindrome("abba"));
+        System.out.println(isPalindrome("loooooooooool"));
+
+    }
+
+    public static boolean isPalindrome(String givenString) {
+        int leng = givenString.length();
+
+        if (leng == 0 || leng == 1) return true;
+        if (givenString.charAt(0) != givenString.charAt(leng-1)) return false;
+
+        String ss = givenString.substring(1,leng-1);
+        return isPalindrome(ss);
+    }
+}
