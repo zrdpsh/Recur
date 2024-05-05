f497314f0758eea060002327aaeb21dbfa5b7f0f
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-05-05 16:47:18 +0300

    Create OnlyEvenIndices.java

---
+++ OnlyEvenIndices.java
@@ -0,0 +1,25 @@
+public class OnlyEvenIndices {
+    public static void main(String[] args) {
+        onlyEvenIndices(new int[]{1, 2, 3, 4, 5}, 0);
+        System.out.println();
+        onlyEvenIndices(new int[]{2, 4, 16, 32, 128}, 0);
+        System.out.println();
+        onlyEvenIndices(new int[]{1, 11, 1111, 111, 1}, 0);
+        System.out.println();
+        onlyEvenIndices(new int[]{1}, 0);
+        System.out.println();
+        onlyEvenIndices(new int[]{},0);
+        System.out.println();
+
+    }
+
+    public static void onlyEvenIndices(int[] givenArray, int index) {
+        if (givenArray.length == index) return;
+
+        int givenNumber = givenArray[index];
+        if (index % 2 == 0) System.out.print(givenNumber + " ");
+
+        onlyEvenIndices(givenArray, index+1);
+    }
+
+}
