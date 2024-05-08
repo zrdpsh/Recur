d105a9b40cd2dd75e41e975ce4c02100266f8481
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-05-09 01:56:05 +0300

    Update StringIsPalindrome.java

--- StringIsPalindrome.java
+++ StringIsPalindrome.java
@@ -1,21 +1,25 @@
 public class StringIsPalindrome {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("", 0));
-        System.out.println(isPalindrome("H", 0));
-        System.out.println(isPalindrome("aba", 0));
-        System.out.println(isPalindrome("google", 0));
-        System.out.println(isPalindrome("abba", 0));
-        System.out.println(isPalindrome("loooooooooool", 0));
+        System.out.println(isPalindrome(""));
+        System.out.println(isPalindrome("H"));
+        System.out.println(isPalindrome("aba"));
+        System.out.println(isPalindrome("google"));
+        System.out.println(isPalindrome("abba"));
+        System.out.println(isPalindrome("loooooooooool"));
 
     }
 
-    public static boolean isPalindrome(String givenString, int beginIndex) {
+    public static boolean isPalindrome(String givenString) {
+        return recurOverString(givenString, 0);
+    }
+
+    private static boolean recurOverString(String givenString, int beginIndex) {
         int leng = givenString.length()-beginIndex;
 
         if (leng == 0 || leng == 1) return true;
         if (givenString.charAt(beginIndex) != givenString.charAt(leng-1)) return false;
 
-        return isPalindrome(givenString, beginIndex+1);
+        return recurOverString(givenString, beginIndex+1);
     }
 }
 
