5c559eac38e4f3bd978ad22298a326f072c0d165
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-05-09 01:57:08 +0300

    Update OnlyEvenIndices.java

--- OnlyEvenIndices.java
+++ OnlyEvenIndices.java
@@ -1,44 +1,29 @@
 public class OnlyEvenIndices {
     public static void main(String[] args) {
-        onlyEvenIndices(new int[]{1, 2, 3, 4, 5}, 0);
+        onlyEvenIndices(new int[]{1, 2, 3, 4, 5});
         System.out.println();
-        onlyEvenIndices(new int[]{2, 4, 16, 32, 128}, 0);
+        onlyEvenIndices(new int[]{2, 4, 16, 32, 128});
         System.out.println();
-        onlyEvenIndices(new int[]{1, 11, 1111, 111, 1}, 0);
+        onlyEvenIndices(new int[]{1, 11, 1111, 111, 1});
         System.out.println();
-        onlyEvenIndices(new int[]{1}, 0);
+        onlyEvenIndices(new int[]{1});
         System.out.println();
-        onlyEvenIndices(new int[]{},0);
+        onlyEvenIndices(new int[]{});
         System.out.println();
 
     }
 
-        public static void onlyEvenIndices(int[] givenArray, int index) {
-        if (givenArray.length >= index) return;
+    public static void onlyEvenIndices(int[] givenArray) {
+        recurOverArray(givenArray, 0);
+    }
+
+    private static void recurOverArray(int[] givenArray, int index) {
+        if (givenArray.length <= index) return;
 
         System.out.print(givenArray[index] + " ");
 
-        onlyEvenIndices(givenArray, index+2);
+        recurOverArray(givenArray, index+2);
     }
-
-
-    
-        public static boolean onlyEvenIndices2(int[] givenArray, int index)
-        {
-            int printedNumber = 0;
-            String numberToString = "";
-
-            boolean a = (index < givenArray.length) && (printedNumber = givenArray[index]) == givenArray[index];
-
-            boolean b = (index < givenArray.length) && (numberToString = "" + printedNumber).equals("" + printedNumber);
-            System.out.print(numberToString + " ");
-
-            boolean c = (index < givenArray.length) && onlyEvenIndices2(givenArray, index+2);
-
-            return a;
-        }
-
-}
 
 
 
